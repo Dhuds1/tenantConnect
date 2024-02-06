3b0761a1719cd85a536c162158eccf561142b33f
Dhuds1 <dhudson1@northislandcollege.ca>
2024-02-06 13:20:23 -0800

    Show userphone number and account info

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -16,13 +16,20 @@
     <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
-
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name"/>
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
-
+        <div>
+            <x-input-label for="user_type" :value="__('Account Type')" />
+            <x-text-input id="user_type" name="user_type" type="text" class="mt-1 block w-full bg-slate-100 text-slate-600 bg-b" :value="old('user_type', $user->user_type)" disabled/>
+        </div>
+        <div>
+            <x-input-label for="phone" :value="__('Phone Number')" />
+            <x-text-input id="name" name="phone" type="tel" class="mt-1 block w-full" :value="old('phone', $user->phone)" required autofocus autocomplete="phone" />
+            <x-input-error class="mt-2" :messages="$errors->get('phone')" />
+        </div>
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
