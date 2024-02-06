5dc3f60f9f1a99906642b4707aa69b9a711a9fb0
Dhuds1 <dhudson1@northislandcollege.ca>
2024-02-06 15:28:18 -0800

    Create a dashboard controller/route

---
+++ app/Http/Controllers/DashboardDirect.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
+
+class DashboardDirect extends Controller
+{
+    public function index(Request $request): View
+    {
+      return view('dashboard', [
+        'user' => $request->user(),
+    ]);
+    }
+}
