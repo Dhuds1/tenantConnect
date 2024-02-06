0dc1f8c3cea0d161bafb810863f1593f188b484b
Dhuds1 <dhudson1@northislandcollege.ca>
2024-02-06 15:48:05 -0800

    add some dashboard sections

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,10 +16,17 @@
         <div class="mt-3 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Tickets Open $user->tickets_open") }}
+                    {{ __("Tickets Open — $user->tickets_open") }}
                 </div>
             </div>
         </div>
-    </div>
+        <div class="mt-3 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Tickets Closed — $user->tickets_closed") }}
+                </div>
+            </div>
+        </div>
+    </div> 
         
 </x-app-layout>
