1650edccde73ae236756e06a99f025e5d8a0e07a
Dhuds1 <dhudson1@northislandcollege.ca>
2024-04-05 19:20:31 -0700

    Updated admin dashboard route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
-    public const ADMIN = '/admin';
+    public const ADMIN = '/admin/dashboard';
     public const TICKET = '/tickets';
 
     /**
