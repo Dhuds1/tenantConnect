b943a79ec9581deb3bd0f4c452232d4277fbeacd
Dhuds1 <dhudson1@northislandcollege.ca>
2024-04-05 19:21:18 -0700

    Concat New to Pending if ticket is new

--- resources/views/livewire/admin/dashboard-shortcuts.blade.php
+++ resources/views/livewire/admin/dashboard-shortcuts.blade.php
@@ -1,3 +1,4 @@
+<?php $new->isEmpty() ?? false ?>
 <div class="mt-3 border-l-4 border-rp-500">
     <div class="w-full text-left text-sm text-gray-500 rtl:text-right rounded-r-lg overflow-hidden">
         @foreach ($tickets as $ticket)
@@ -5,7 +6,7 @@
         <?php $route = route('ad.ticket.viewing',['id' => "$ticket->id"]); ?>
         <div class="odd:bg-gray-100 even:bg-gray-200 flex justify-between">
             <div class="whitespace-nowrap p-4 font-medium text-gray-900" scope="row">
-                {{ $ticket->tenant }}
+                {{ $ticket->tenant }} {{$new === true ?? $new[$ticket->id] ? "- New" : ''}}
             </div>
             <div class="p-4">
                 <a class="font-medium p-2 rounded text-ob-900 hover:text-rp-500 transition-all duration-200"
